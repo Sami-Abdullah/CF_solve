03d23204131b7e98070a60da64905d910dcec24e
Sami-Abdullah <sami38596@gmail.com>
2024-10-26 18:28:07 +0600

    check

--- 1998-B-Minimize-Equal-Sum-Subarrays.cpp
+++ 1998-B-Minimize-Equal-Sum-Subarrays.cpp
@@ -10,16 +10,15 @@ int main (){
         for(int i=0;i<size;i++){
             cin>>v[i];
         }
-        if(size<4){
-            swap(v[0],v[1]);
-        }
-        else{
-            for(int i=2;i<size-2;i=i+2){
-                swap(v[i],v[i+1]);
+        for(int i=0;i<size;i++){
+            if(v[i]==size)v[i]=1;
+            else{
+                v[i]=v[i]%size+1;
             }
         }
-        for(int i=0;i<size;i++)cout<<v[i]<<" ";
+        for(int i=0;i<size;i++){
+            cout<<v[i]<<" ";
+        }
         cout<<endl;
-
     }
 }
